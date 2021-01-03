e9e43fbd09764a838bf62259a3f571b24f3cfb09
Ivan Pletinskiy <ivanhanden@gmail.com>
2021-01-03 16:59:38 +0300

    Setup bottom navigation with viewPager2

---
+++ app/src/main/java/com/handen/cookassistant/presentation/util/TabsAdapter.kt
@@ -0,0 +1,22 @@
+package com.handen.cookassistant.presentation.util
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.handen.cookassistant.presentation.ui.dashboard.DashboardFragment
+import com.handen.cookassistant.presentation.ui.home.HomeFragment
+import com.handen.cookassistant.presentation.ui.notifications.NotificationsFragment
+
+class TabsAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount() = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> DashboardFragment()
+            1 -> HomeFragment()
+            2 -> NotificationsFragment()
+            else -> throw IllegalArgumentException("TabsAdapter position > fragmentsCount")
+        }
+    }
+}
